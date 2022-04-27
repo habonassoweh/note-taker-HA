a7e86373fdc7868141c5945bd32611fe842e47b0
Habon Assoweh <habonassoweh@Habons-Air>
2022-04-27 19:16:34 -0400

    readme

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 
-const port = process.env.port || 3000;
+
+const port = process.env.port || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 //app.use("/api");
-//app.use("/");
+app.use("/", htmlRoutes);
 app.listen(port, () => console.log(`listening on port: ${port}`));
