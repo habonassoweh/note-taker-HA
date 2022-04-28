0863f90d1bfff748db150ae105a8463944fbe1d6
Habon Assoweh <habonassoweh@Habons-Air>
2022-04-27 20:21:13 -0400

    final updates

--- Develop/server.js
+++ Develop/server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 
-
 const port = process.env.port || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-//app.use("/api");
+app.use("/api");
 app.use("/", htmlRoutes);
 app.listen(port, () => console.log(`listening on port: ${port}`));
