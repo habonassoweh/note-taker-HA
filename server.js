508026899f41e63046f12b3a6b793e436b07bcce
Habon Assoweh <habonassoweh@Habons-Air>
2022-04-29 21:19:07 -0400

    heroku deployed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.listen(PORT, () => {
-  console.log("API server now on port ${PORT}!");
+  console.log(`API server now on port ${PORT}!`);
 });
 
 function filterByQuery(query, notesArray) {
